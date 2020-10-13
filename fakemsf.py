11ad29d29fe32aff7f04556fc2b396cf37e5ff8b
John Hammond <johnhammond010@gmail.com>
2020-10-12 21:13:29 -0400

    Started to "search" functionality. Found pymetasploit3

--- fakemsf.py
+++ fakemsf.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 
+
+"""
+examine_module will exract out the information from a module
+Now we just need to transform it into a Python object and parse it
+
+https://github.com/DanMcInerney/pymetasploit3
+
+"""
+
+import re
 import cmd2
 import time
 import sys
 import os
 import shutil
 import subprocess
+import argparse
 from colorama import *
 from glob import glob
 import random
 from pprint import pprint
+import prettytable
 
 def load_logos():
     """
@@ -71,6 +83,20 @@ def load_modules():
     return modules
 
 
+def examine_module(module_path):
+
+    if not os.path.isfile(module_path):
+        return None
+
+    with open(module_path) as h:
+        bounds = re.search("super\(\s+update_info\((.+?)\)\s*?\)", h.read(), re.MULTILINE | re.DOTALL)
+        if bounds:
+            return bounds.group()
+
+
+# This really needs to be brought into a more formal class/OOP...
+modules = load_modules()
+
 class App(cmd2.Cmd):
 
     def __init__(self):
@@ -116,17 +142,62 @@ class App(cmd2.Cmd):
         else:
             sys.stderr.write(f"{Style.BRIGHT+Fore.RED}[-]{Style.NORMAL+Fore.RESET} {msg}" + end)
 
-
     def sigint_handler(self, signum: int, frame):
         """
         Handle Ctrl+C interrupts like msfconsole does
         """
         self.async_alert(f"{self.prompt}Interrupt: use the 'exit' command to quit")
 
-
     def do_exit(self, args):
         return True
 
+
+
+
+
+    """
+    This can be put together more formally later...
+    """
+    search_parser = argparse.ArgumentParser()
+    search_parser.add_argument('options', help='options info')
+    search_parser.add_argument('-u', action='store_true', help='Use module if there is one result')
+    @cmd2.with_argparser(search_parser)
+    def do_search(self, args):
+        """
+        Prepending a value with '-' will exclude any matching results.
+        If no options or keywords are provided, cached results are displayed.
+        """
+
+        results = []
+        search = args.options
+
+        for key, value in modules.items():
+            for m in value:
+                if search in m:
+                    results.append(m)
+        
+        """
+        This display table isn't perfect, but it is good enough
+        for now.
+        """
+        if results:
+            
+            self.stdout.write("\nMatching Modules\n"+"="*16+"\n\n")
+
+            display = prettytable.PrettyTable()
+            display.align = "l"
+            display.field_names = [ "#", "Name", "Disclosure Date", "Rank", "Check", "Description" ]
+            
+            for i, result in enumerate(results):
+                display.add_row([i, result.rstrip(".rb"), "to-do","to-do","to-do","to-do"])
+
+            print(display.get_string(border=True, hrules=prettytable.HEADER, vertical_char=" ", junction_char=" ").replace("|"," "))
+            
+
+            self.stdout.write("\n\n")            
+            self.stdout.write(f"Interact with a module by name or index. For example {Fore.GREEN}info 5{Fore.RESET}, {Fore.GREEN}use 5{Fore.RESET} or {Fore.GREEN}use exploit/windows/smb/smb_doublepulsar_rce{Fore.RESET}\n\n")
+        else:
+            self.perror("No results from search")
 
 def startup():
     """
@@ -154,6 +225,6 @@ def startup():
 
 
 # startup()
-app = App()
-app.cmdloop()
-
+# app = App()
+# app.cmdloop()
+print(examine_module("modules/exploits/windows/smb/ms17_010_eternalblue.rb"))
